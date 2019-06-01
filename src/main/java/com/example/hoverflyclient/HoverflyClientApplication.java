cf1b13db1a917153dd6756f3e3b1de42ce52a9f3
Hanumantharao <hanu.satuluri@gmail.com>
2019-06-01 16:27:57 +0530

    Hoverfly Client

---
+++ src/main/java/com/example/hoverflyclient/HoverflyClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoverflyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoverflyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoverflyClientApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/hoverflyclient/controller/MyClientRestController.java
@@ -0,0 +1,23 @@
+package com.example.hoverflyclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+public class MyClientRestController {
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@RequestMapping("/invoke")
+	public String invoke() {
+		
+		System.out.println("inside Test Controller::invoke()");
+		
+		String url="http://localhost:9080/service/hoverfly";
+		
+		String response = restTemplate.
+		
+	}
+
+}
